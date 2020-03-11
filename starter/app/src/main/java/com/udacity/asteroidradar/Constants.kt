4b9b843e1e2328b5ff8957087c2a9d69408e60bd
Oluwasegun Wahaab <abrolu@yahoo.com>
2020-03-11 15:52:06 +0100

    removed api key

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/neo/rest/v1/"
-    const val API_KEY = "lSeto9bOnRSCKouwfX0XRR4FmGy2oStdv6DDZ3pI"
+    const val API_KEY = "YOUR_API_KEY_HERE"
     const val IMAGE_BASE_URL = "https://api.nasa.gov/planetary/"
     const val IMAGE = "image"
     const val MILLIS_IN_A_DAY = 86400000L
